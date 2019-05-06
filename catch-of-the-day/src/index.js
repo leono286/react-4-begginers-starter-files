ea4aae79b00f0f73fb5f1391237629d79e1e2fa3
Leonardo Perez <leono286@gmail.com>
2019-05-05 21:51:58 -0500

    up to video 12

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from "react";
+import "./css/style.css";
+import { render } from "react-dom";
+import Router from "./components/Router";
+
+render(<Router />, document.querySelector('#main'));
